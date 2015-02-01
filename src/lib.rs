81b9f6d070dab6fc452b34a2e13cbcff2d9d363c
Andrew Gallant <jamslam@gmail.com>
2015-02-01 15:52:08 -0500

    Some basic types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,26 @@
 #![crate_name = "cbor"]
 #![doc(html_root_url = "http://burntsushi.net/rustdoc/cbor")]
+
+use std::collections::HashMap;
+
+pub enum Cbor {
+    Primitive(CborPrimitive),
+    Array(Vec<Cbor>),
+    Map(HashMap<CborPrimitive, Cbor>),
+    Tag(Box<Cbor>),
+}
+
+pub enum CborPrimitive {
+    Unsigned(CborUnsigned),
+    Signed(CborSigned),
+    Float(CborFloat),
+    Bytes(Vec<u8>),
+    Unicode(String),
+}
+
+#[derive(Copy)]
+pub enum CborUnsigned { UInt8(u8), UInt16(u16), UInt32(u32), UInt64(u64) }
+#[derive(Copy)]
+pub enum CborSigned { Int8(i8), Int16(i16), Int32(i32), Int64(i64) }
+#[derive(Copy)]
+pub enum CborFloat { Float32(f32), Float64(f64) }
