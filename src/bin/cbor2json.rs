1ca98ee640c4b7583540e040fc29c7a51b635d5c
Andrew Gallant <jamslam@gmail.com>
2015-02-15 12:30:16 -0500

    CLI tools for converting between CBOR and JSON.

---
+++ src/bin/cbor2json.rs
@@ -0,0 +1,35 @@
+#![feature(env, io)]
+
+extern crate cbor;
+extern crate "rustc-serialize" as rustc_serialize;
+
+use std::env::set_exit_status;
+use std::old_io as io;
+
+use cbor::Decoder;
+use rustc_serialize::json::ToJson;
+
+macro_rules! err {
+    ($($arg:tt)*) => ({
+        let _ = io::stderr().write_str(&*format!($($arg)*));
+        let _ = io::stderr().write_str("\n");
+    });
+}
+
+fn main() {
+    macro_rules! ordie {
+        ($e:expr) => (
+            match $e {
+                Ok(v) => v,
+                Err(err) => { err!("{}", err); set_exit_status(1); return; }
+            }
+        );
+    }
+    let mut dec = Decoder::from_reader(io::stdin());
+    let cbor = match dec.items().next() {
+        None => { return; },
+        Some(result) => ordie!(result),
+    };
+    let json = cbor.to_json().pretty().to_string();
+    print!("{}", json);
+}
---
+++ src/bin/json2cbor.rs
@@ -0,0 +1,31 @@
+#![feature(env, io)]
+
+extern crate cbor;
+extern crate "rustc-serialize" as rustc_serialize;
+
+use std::env::set_exit_status;
+use std::old_io as io;
+
+use cbor::{Encoder, ToCbor};
+use rustc_serialize::json::Json;
+
+macro_rules! err {
+    ($($arg:tt)*) => ({
+        let _ = io::stderr().write_str(&*format!($($arg)*));
+        let _ = io::stderr().write_str("\n");
+    });
+}
+
+fn main() {
+    macro_rules! ordie {
+        ($e:expr) => (
+            match $e {
+                Ok(v) => v,
+                Err(err) => { err!("{}", err); set_exit_status(1); return; }
+            }
+        );
+    }
+    let json = ordie!(Json::from_reader(&mut io::stdin()));
+    let mut enc = Encoder::from_writer(io::stdout());
+    ordie!(enc.encode(&[json.to_cbor()]));
+}
