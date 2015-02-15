35a9313ecc9b7514ce6b5f1752ec72ffa649edf2
Andrew Gallant <jamslam@gmail.com>
2015-02-15 14:14:04 -0500

    Update README and benchmarks.

--- README.md
+++ README.md
@@ -1,4 +1,134 @@
-An implementation of [CBOR](https://tools.ietf.org/html/rfc7049) in Rust.
+This crate provides an implementation of [RFC
+7049](https://tools.ietf.org/html/rfc7049), which specifies Concise Binary
+Object Representation (CBOR). CBOR adopts and modestly builds on the *data
+model* used by JSON, except the encoding is in binary form. Its primary goals
+include a balance of implementation size, message size and extensibility.
 
-Work in progress.
+[![Build status](https://api.travis-ci.org/BurntSushi/rust-cbor.png)](https://travis-ci.org/BurntSushi/rust-cbor)
 
+Licensed under the [UNLICENSE](http://unlicense.org).
+
+
+### Documentation
+
+The API is fully documented with examples:
+[http://burntsushi.net/rustdoc/cbor/](http://burntsushi.net/rustdoc/cbor/).
+
+
+### Installation
+
+This crate works with Cargo and is on
+[crates.io](https://crates.io/crates/cbor). The package is regularly updated.
+Add is to your `Cargo.toml` like so:
+
+```toml
+[dependencies]
+cbor = "*"
+```
+
+
+### Example: simple type based encoding and decoding
+
+In this crate, there is a `Decoder` and an `Encoder`. All reading and writing
+of CBOR must go through one of these types.
+
+The following shows how use those types to encode and decode a sequence of data
+items:
+
+```rust
+extern crate cbor;
+
+use cbor::{Decoder, Encoder};
+
+fn main() {
+    // The data we want to encode. Each element in the list is encoded as its
+    // own separate top-level data item.
+    let data = vec![('a', 1), ('b', 2), ('c', 3)];
+
+    // Create an in memory encoder. Use `Encoder::from_writer` to write to
+    // anything that implements `Writer`.
+    let mut e = Encoder::from_memory();
+    e.encode(&data).unwrap();
+
+    // Create an in memory decoder. Use `Decoder::from_reader` to read from
+    // anything that implements `Reader`.
+    let mut d = Decoder::from_bytes(e.as_bytes());
+    let items: Vec<(char, i32)> = d.decode().collect::<Result<_, _>>().unwrap();
+
+    assert_eq!(items, data);
+}
+```
+
+
+### Status of implementation
+
+The big thing missing at the moment is indefinite length encoding. It's easy
+enough to add, but I'm still trying to think of the best way to expose it in
+the API.
+
+Otherwise, all core CBOR features are implemented. There is support for tags,
+but none of the tags in the IANA registry are implemented. It isn't clear to me
+whether these implementations should appear in this crate or in others. Perhaps
+this would be a good use of Cargo's optional features.
+
+Finally, CBOR maps are only allowed to have Unicode string keys. This was
+easiest to implement, but perhaps this restriction should be lifted in the
+future.
+
+
+### Benchmarks
+
+Here are some very rough (and too simplistic) benchmarks that compare CBOR with
+JSON. Absolute performance is pretty bad (sans CBOR encoding), but this should
+at least give a good ballpark for relative performance with JSON:
+
+```
+test decode_medium_cbor   ... bench:  15525074 ns/iter (+/- 348424) = 25 MB/s
+test decode_medium_json   ... bench:  18356213 ns/iter (+/- 620645) = 30 MB/s
+test decode_small_cbor    ... bench:      1299 ns/iter (+/- 6) = 30 MB/s
+test decode_small_json    ... bench:      1471 ns/iter (+/- 11) = 38 MB/s
+test encode_medium_cbor   ... bench:   1379671 ns/iter (+/- 24828) = 289 MB/s
+test encode_medium_json   ... bench:   8053979 ns/iter (+/- 110462) = 70 MB/s
+test encode_medium_tojson ... bench:  15589704 ns/iter (+/- 559355) = 36 MB/s
+test encode_small_cbor    ... bench:      2685 ns/iter (+/- 69) = 14 MB/s
+test encode_small_json    ... bench:       862 ns/iter (+/- 1) = 64 MB/s
+test encode_small_tojson  ... bench:      1313 ns/iter (+/- 6) = 42 MB/s
+test read_medium_cbor     ... bench:  10008308 ns/iter (+/- 101995) = 39 MB/s
+test read_medium_json     ... bench:  14853023 ns/iter (+/- 510215) = 38 MB/s
+test read_small_cbor      ... bench:       763 ns/iter (+/- 4) = 52 MB/s
+test read_small_json      ... bench:      1127 ns/iter (+/- 4) = 49 MB/s
+```
+
+If these benchmarks are perplexing to you, then you might want to check out
+[Erick Tryzelaar's series of blog
+posts](http://erickt.github.io/blog/2014/10/28/serialization/)
+on Rust's serialization infrastructure. In short,
+[it's being worked on](https://github.com/erickt/rust-serde).
+
+Relatedly, a compounding reason why decoding CBOR is so slow is because it is
+decoded into an intermediate abstract syntax first. A faster (but more complex)
+implementation would skip this step, but it is difficult to do performantly
+with the existing serialization infrastructure. (The same approach is used
+in JSON decoding too, but it should be much easier to eschew this with CBOR
+since it doesn't have the complexity overhead of parsing text.)
+
+
+### Alternatives
+
+[TyOverby's excellent `bincode`](https://github.com/TyOverby/bincode) library
+fulfills a similar use case as `cbor`: both crates serialize and deserialize
+between Rust values and a binary representation. Here is a brief comparison
+(please ping me if I've gotten any of this wrong or if I've left out other
+crucial details):
+
+* CBOR is an IETF standard with implementations in
+  [many languages](http://cbor.io/impls.html).
+* `cbor` tags every data item encoded, including every number. `bincode` does
+  not, which means the compactness of the resulting binary data depends on your
+  data. For example, using `cbor`, encoding a `Vec<u64>` will encode every
+  integer using a variable width encoding while `bincode` will use 8 bytes for
+  every number. This results in various trade offs in terms of serialization
+  speed, the size of the data and the flexibility of encoding/decoding with
+  Rust types. (e.g., With `bincode` you must decode with precisely the same
+  integer size as what was encoded, but `cbor` can adjust on the fly and
+  decode, e.g., an encoded `u16` into a `u64`.)
---
+++ tests/bench.rs
@@ -0,0 +1,201 @@
+#![feature(test)]
+
+extern crate cbor;
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate test;
+
+use std::iter::repeat;
+use cbor::{Decoder, Encoder};
+use rustc_serialize::Encodable;
+use rustc_serialize::json::{self, Json, ToJson};
+
+fn cbor_encode<T: Encodable>(v: T) -> Vec<u8> {
+    let mut enc = Encoder::from_memory();
+    enc.encode(&[v]).unwrap();
+    enc.as_bytes().to_vec()
+}
+
+#[bench]
+fn encode_small_cbor(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+
+    b.bytes = cbor_encode(&data).len() as u64;
+    b.iter(|| {
+        cbor_encode(&data);
+    });
+}
+
+#[bench]
+fn encode_small_json(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+
+    b.bytes = data.to_json().to_string().len() as u64;
+    b.iter(|| {
+        json::encode(&data).unwrap();
+    });
+}
+
+#[bench]
+fn encode_small_tojson(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+
+    b.bytes = data.to_json().to_string().len() as u64;
+    b.iter(|| {
+        data.to_json().to_string();
+    });
+}
+
+#[bench]
+fn encode_medium_cbor(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let items = repeat(&data).take(10_000).collect::<Vec<_>>();
+
+    b.bytes = cbor_encode(&items).len() as u64;
+    b.iter(|| {
+        cbor_encode(&items);
+    });
+}
+
+#[bench]
+fn encode_medium_json(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let items = repeat(data).take(10_000).collect::<Vec<_>>();
+
+    b.bytes = items.to_json().to_string().len() as u64;
+    b.iter(|| {
+        json::encode(&items).unwrap();
+    });
+}
+
+#[bench]
+fn encode_medium_tojson(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let items = repeat(data).take(10_000).collect::<Vec<_>>();
+
+    b.bytes = items.to_json().to_string().len() as u64;
+    b.iter(|| {
+        items.to_json().to_string();
+    });
+}
+
+#[bench]
+fn read_small_cbor(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let mut enc = Encoder::from_memory();
+    enc.encode(&[data]).unwrap();
+    let bytes = enc.as_bytes();
+
+    b.bytes = bytes.len() as u64;
+    b.iter(|| {
+        let mut dec = Decoder::from_bytes(bytes.to_vec());
+        dec.items().next().unwrap().unwrap();
+    });
+}
+
+#[bench]
+fn read_small_json(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let string = json::encode(&data).unwrap();
+
+    b.bytes = string.len() as u64;
+    b.iter(|| {
+        Json::from_str(&string).unwrap();
+    });
+}
+
+#[bench]
+fn read_medium_cbor(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let items = repeat(data).take(10_000).collect::<Vec<_>>();
+    let mut enc = Encoder::from_memory();
+    enc.encode(&[items]).unwrap();
+    let bytes = enc.as_bytes();
+
+    b.bytes = bytes.len() as u64;
+    b.iter(|| {
+        let mut dec = Decoder::from_bytes(bytes.to_vec());
+        dec.items().next().unwrap().unwrap();
+    });
+}
+
+#[bench]
+fn read_medium_json(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let items = repeat(data).take(10_000).collect::<Vec<_>>();
+    let string = json::encode(&items).unwrap();
+
+    b.bytes = string.len() as u64;
+    b.iter(|| {
+        Json::from_str(&string).unwrap();
+    });
+}
+
+#[bench]
+fn decode_small_cbor(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let mut enc = Encoder::from_memory();
+    enc.encode(&[data]).unwrap();
+    let bytes = enc.as_bytes();
+
+    b.bytes = bytes.len() as u64;
+    b.iter(|| {
+        let mut dec = Decoder::from_bytes(bytes.to_vec());
+        let _: (String, bool, (), Vec<u64>, f64) =
+            dec.decode().next().unwrap().unwrap();
+    });
+}
+
+#[bench]
+fn decode_small_json(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let string = json::encode(&data).unwrap();
+
+    b.bytes = string.len() as u64;
+    b.iter(|| {
+        let _: (String, bool, (), Vec<u64>, f64) =
+            json::decode(&string).unwrap();
+    });
+}
+
+#[bench]
+fn decode_medium_cbor(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let items = repeat(data).take(10_000).collect::<Vec<_>>();
+    let mut enc = Encoder::from_memory();
+    enc.encode(&[items]).unwrap();
+    let bytes = enc.as_bytes();
+
+    b.bytes = bytes.len() as u64;
+    b.iter(|| {
+        let mut dec = Decoder::from_bytes(bytes.to_vec());
+        let _: Vec<(String, bool, (), Vec<u64>, f64)> =
+            dec.decode().next().unwrap().unwrap();
+    });
+}
+
+#[bench]
+fn decode_medium_json(b: &mut test::Bencher) {
+    let data = ("hello, world".to_string(),
+                true, (), vec![1, 1000, 100_000, 10_000_000], 3.14);
+    let items = repeat(data).take(10_000).collect::<Vec<_>>();
+    let string = json::encode(&items).unwrap();
+
+    b.bytes = string.len() as u64;
+    b.iter(|| {
+        let _: Vec<(String, bool, (), Vec<u64>, f64)> =
+            json::decode(&string).unwrap();
+    });
+}
